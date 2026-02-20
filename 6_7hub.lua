450161000eb1c870e21e94f9e346ba05f8ce865a
hizuma_67 <bbuichihieu@gmail.com>
2026-02-20 13:16:53 +0700

    Update 6_7hub.lua

--- 6_7hub.lua
+++ 6_7hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hizuma-67/all/refs/heads/main/3_6hub.obfuscated.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hizuma-67/all/refs/heads/main/obfuscated_script-1771568101029.lua.txt"))()
