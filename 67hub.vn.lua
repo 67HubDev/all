145ab32e7b9aa46258ecf2a0a0dad92936dbf2f5
hizuma_67 <bbuichihieu@gmail.com>
2026-02-25 18:41:04 +0700

    Update 67hub.vn.lua

--- 67hub.vn.lua
+++ 67hub.vn.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/67HubDev/oioi/refs/heads/main/obfuscated_script-1771950430402.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/67HubDev/67hub/refs/heads/main/obfuscated_script-1772019565578.lua.txt"))()
